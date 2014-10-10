2eb6d26ac9bc1dcd038e9e0798d26974e23ca9a2
shuji <watanabe.shuji@classmethod.jp>
2014-10-10 14:25:22 +0900

    ElasticLoadBalancing対応

--- src/main/groovy/jp/classmethod/aws/cloudformation/Template.groovy
+++ src/main/groovy/jp/classmethod/aws/cloudformation/Template.groovy
@@ -39,6 +39,7 @@ class Template {
         dir.listFiles({d,f -> f ==~ /\d+_SecurityGroup\.csv/ } as FilenameFilter).each { SecurityGroup.inject(resources, it) }
         dir.listFiles({d,f -> f ==~ /\d+_Volume\.csv/ } as FilenameFilter).each { Volume.inject(resources, it) }
         dir.listFiles({d,f -> f ==~ /\d+_Instance\.csv/ } as FilenameFilter).each { Instance.inject(resources, it) }
+        dir.listFiles({d,f -> f ==~ /\d+_ElasticLoadBalancing\.csv/ } as FilenameFilter).each { ElasticLoadBalancing.inject(resources, it) }
         dir.listFiles({d,f -> f ==~ /\d+_DBSubnetGroup\.csv/ } as FilenameFilter).each { DBSubnetGroup.inject(resources, it) }
         dir.listFiles({d,f -> f ==~ /\d+_DBInstance\.csv/ } as FilenameFilter).each { DBInstance.inject(resources, it) }
         dir.listFiles({d,f -> f ==~ /\d+_CacheSubnetGroup\.csv/ } as FilenameFilter).each { CacheSubnetGroup.inject(resources, it) }
